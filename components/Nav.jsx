1055fb5b119435adaf95dfde8682320bbfc9fc42
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-10-26 19:52:11 +0000

    struture all navbar pages and finish navbar jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -20,10 +20,32 @@ const links = [
     name: "work",
     path: "/work",
   },
+  {
+    name: "contact",
+    path: "/contact",
+  },
 ];
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathname && "text-accent border-b-2 border-accent"
+            } capitalize font-medium hover:text-accent transition-all `}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
