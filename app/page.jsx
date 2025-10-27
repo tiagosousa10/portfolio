16ebda16b68317ea45a68504e474f1d52816dabb
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-10-27 21:17:30 +0000

    photo component added

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,16 @@
-import Socials from "@/components/Socials";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
+
+// components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 const Home = () => {
   return (
     <div className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Tiago Sousa</span>
@@ -27,12 +30,19 @@ const Home = () => {
                 <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Socials />
+                <Social
+                  containerStyles={"flex gap-6"}
+                  iconStyles={
+                    "w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  }
+                />
               </div>
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </div>
