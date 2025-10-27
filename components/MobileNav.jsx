ca2b72bdb781c028b23a53b7ca1176e09586a385
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-10-27 16:20:18 +0000

    create navbar for mobile & update sheet component with new style

---
+++ components/MobileNav.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div>logo</div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
